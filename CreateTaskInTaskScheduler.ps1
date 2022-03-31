56a302800a987badb66df1278c5980062f0441ae
handshakings <60043564+handshakings@users.noreply.github.com>
2022-03-31 14:42:46 +0500

    Create Task using Powershell

---
+++ CreateTaskInTaskScheduler.ps1
@@ -0,0 +1,3 @@
+﻿$action = New-ScheduledTaskAction -Execute 'C:\Windows\System32\notepad.exe'
+$trigger = New-ScheduledTaskTrigger -Daily -At 14:42pm
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "MyTask" 
